207ef61c4d0428729033f571c315761571a3cff7
Cindy He <che@ncsu.edu>
2011-09-25 12:33:23 -0500

    -m Add admin column to user table, now page can identify admin user by current_user.admin?

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110925050710) do
+ActiveRecord::Schema.define(:version => 20110925155926) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -36,8 +36,8 @@ ActiveRecord::Schema.define(:version => 20110925050710) do
     t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "email",                                 :default => "",    :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(:version => 20110925050710) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.boolean  "admin",                                 :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
