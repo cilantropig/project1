4b03a398422dc9c165c5bbde83cfe8237fbb89fd
Aldo Valenti <usaalv@gmail.com>
2011-09-30 21:31:13 -0400

    sd

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,11 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :replies, :dependent => :destroy
   has_many :votes, :dependent => :destroy
+<<<<<<< HEAD
   validates :title, :body, :presence => true
+=======
+
+  validates :title, :body, :presence => true
+
+>>>>>>> master
 end
