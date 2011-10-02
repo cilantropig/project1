4fb0bf5166f288527c955cb0ed4f47b8c8cf231b
Jeremy Aube <jjaube@ncsu.edu>
2011-10-02 03:38:11 -0400

    Sorts posts by weight, which the age of the post in hours minus the number of votes. Any post older than 10 days (including any extension via votes) is not displayed.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,20 @@ class Post < ActiveRecord::Base
 
   validates :title, :body, :presence => true
 
+  public
+
+  def active?
+    return weight < 240 # hours in 10 days
+  end
+
+  def weight
+    time = Time.now
+    dif = time - self.created_at
+    seconds_in_an_hour = 3600
+    seconds = dif / seconds_in_an_hour
+
+    # each vote extends post life by one hour
+    return seconds - self.votes.count
+  end
+
 end
