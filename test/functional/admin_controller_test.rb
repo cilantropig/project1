16e5e312e9097deed854b5b85b1c77c3f70a7c59
Cindy He <che@ncsu.edu>
2011-10-05 21:24:03 -0500

    remove manage report from admin controller test

--- test/functional/admin_controller_test.rb
+++ test/functional/admin_controller_test.rb
@@ -13,11 +13,6 @@ class AdminControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get manage_posts" do
-    get :manage_posts
-    assert_response :success
-  end
-
   test "should get view_reports" do
     get :view_reports
     assert_response :success
